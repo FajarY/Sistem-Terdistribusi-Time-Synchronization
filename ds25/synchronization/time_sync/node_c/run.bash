49e834ce08628ca91b84dfc501e2cdb14fbed7ec
FajarY <fajaryasodana.dev@gmail.com>
2025-11-03 15:27:43 +0700

    feat: Update

--- ds25/synchronization/time_sync/node_c/run.bash
+++ ds25/synchronization/time_sync/node_c/run.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 python3 peer_node.py \
-	  --name A --listen 0.0.0.0 5000 \
+	  --name C --listen 0.0.0.0 5002 \
 	  --peers A@192.168.122.204:5000 B@192.168.122.150:5001 C@localhost:5002 D@192.168.122.234:5003 \ 
 	  --logger 192.168.122.100 9999 \
-	  --offset-ms 600 \
-	  --initiate-broadcast --msg "Hello from A"
+	  --offset-ms 600
 
